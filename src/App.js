4c7519149fc3fe4ccc33848332f2221ce386355e
Ronald Estupiñan <ronaldestupinan@ZCOBTMH100224-RonaldEstupinan.local>
2019-10-07 19:18:49 -0500

    parrah

--- src/App.js
+++ src/App.js
@@ -8,14 +8,17 @@ class App extends Component {
     this.handleChecked = this.handleChecked.bind(this); // set this, because you need get methods from CheckBox 
   }
   handleChecked () {
-    this.setState({isChecked: !this.state.isChecked});
+
+    if (this.state.isChecked) {
+        this.setState({testCheck:''});
+        this.setState({isChecked: !this.state.isChecked});
+    }else{
+        this.setState({testCheck: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'})
+        this.setState({isChecked: !this.state.isChecked});
+    }
   }
   render() {
-    if (this.state.isChecked) {
-      this.state.testCheck = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
-    }else{
-      this.state.testCheck = ''
-    }
+    
     return (
       <div className="wrapper">
         <label><input type="checkbox"  onChange={ this.handleChecked }/> Mostrar información importante</label>
@@ -26,4 +29,5 @@ class App extends Component {
 }
 
 export default App;
+
 
