c4c31b0b49680a03aa9f21951ba5bf7ee4770f9b
Ronald Estupiñan <ronaldestupinan@ZCOBTMH100224-RonaldEstupinan.local>
2019-10-07 19:31:23 -0500

    fixing <p>

--- src/App.js
+++ src/App.js
@@ -11,18 +11,18 @@ class App extends Component {
 
     if (this.state.isChecked) {
         this.setState({testCheck:''});
-        this.setState({isChecked: !this.state.isChecked});
     }else{
         this.setState({testCheck: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'})
-        this.setState({isChecked: !this.state.isChecked});
     }
+    this.setState({isChecked: !this.state.isChecked});
+
   }
   render() {
     
     return (
       <div className="wrapper">
         <label><input type="checkbox"  onChange={ this.handleChecked }/> Mostrar información importante</label>
-        <p>{this.state.testCheck}</p>
+        {this.state.isChecked && (<p>{this.state.testCheck}</p>)}
       </div>
     );
   }
